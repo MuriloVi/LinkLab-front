95cd6b987f42632e6c7a95dacbe6cd06f21346f0
MuriloVi <murilovif17@gmail.com>
2020-09-05 13:25:29 -0300

    Add estilização simples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo from './assets/LinkLab.png'
+
 import './App.css';
+import api from './services/api';
+
 
 function App() {
+
+  function HandleForm(e){
+    e.preventDefault()
+    console.log('OK!')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home-container">
+      <div className="logo-top"><img alt="" src={Logo}/></div>
+      <div className="input-box">
+      <form onSubmit={HandleForm}>
+      <input placeholder="URL"/>
+      <button type="submit">Encolher</button>
+     </form>
+
+      </div>
+    
     </div>
   );
 }
